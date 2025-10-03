c81e8ad4b2062a2112cf0e617251ee816db29392
tarniq <thales.cuellas@gmail.com>
2025-10-03 18:47:12 -0300

    feat: Add Lombok and anotations, @NoArgsConstructor in NinjaModel and MissoesModel

--- cadastroNinjas/src/main/java/dev/java10x/cadastroNinjas/Missoes/MissoesModel.java
+++ cadastroNinjas/src/main/java/dev/java10x/cadastroNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.java10x.cadastroNinjas.Missoes;
 
 import dev.java10x.cadastroNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_Missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,46 +24,4 @@ public class MissoesModel {
     // Uma missao pode ter varios ninjas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
-
-    public MissoesModel() {
-    }
-
-    public MissoesModel(String nome, double credito, Rank rank, List<NinjaModel> ninjas) {
-        this.nome = nome;
-        this.credito = credito;
-        this.rank = rank;
-        this.ninjas = ninjas;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public double getCredito() {
-        return credito;
-    }
-
-    public void setCredito(double credito) {
-        this.credito = credito;
-    }
-
-    public Rank getRank() {
-        return rank;
-    }
-
-    public void setRank(Rank rank) {
-        this.rank = rank;
-    }
-
-    public List<NinjaModel> getNinjas() {
-        return ninjas;
-    }
-
-    public void setNinjas(List<NinjaModel> ninjas) {
-        this.ninjas = ninjas;
-    }
 }
--- cadastroNinjas/src/main/java/dev/java10x/cadastroNinjas/Ninjas/NinjaModel.java
+++ cadastroNinjas/src/main/java/dev/java10x/cadastroNinjas/Ninjas/NinjaModel.java
@@ -3,6 +3,9 @@ package dev.java10x.cadastroNinjas.Ninjas;
 
 import dev.java10x.cadastroNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +14,9 @@ import java.util.List;
 // Transforma uma classe em uma entidade de um banco de dados
 @Entity
 @Table(name= "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,45 +30,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_ID") //chave estrangeira - foreing key
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, int idade, String email, MissoesModel missoes) {
-        this.nome = nome;
-        this.idade = idade;
-        this.email = email;
-        this.missoes = missoes;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public MissoesModel getMissoes() {
-        return missoes;
-    }
-
-    public void setMissoes(MissoesModel missoes) {
-        this.missoes = missoes;
-    }
 }
