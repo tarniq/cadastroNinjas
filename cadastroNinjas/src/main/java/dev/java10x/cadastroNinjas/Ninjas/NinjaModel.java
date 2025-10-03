e5dc1a9a4d2516dc255b1811c9327895b20c7579
tarniq <thales.cuellas@gmail.com>
2025-10-03 19:01:24 -0300

    fix: Add @Colum unique in NinjaModel email

--- cadastroNinjas/src/main/java/dev/java10x/cadastroNinjas/Ninjas/NinjaModel.java
+++ cadastroNinjas/src/main/java/dev/java10x/cadastroNinjas/Ninjas/NinjaModel.java
@@ -23,6 +23,8 @@ public class NinjaModel {
     private Long id;
     private String nome;
     private int idade;
+
+    @Column(unique = true)
     private String email;
 
     // Um ninja tem uma unica missao
