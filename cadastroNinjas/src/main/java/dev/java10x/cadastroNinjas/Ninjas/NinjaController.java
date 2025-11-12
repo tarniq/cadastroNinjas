9e0827276b624933d5c8d869d8137e5a5e0a15f0
tarniq <thales.cuellas@gmail.com>
2025-11-11 21:01:49 -0300

    feat: Atualiza Ninja controller com mock de dados

--- cadastroNinjas/src/main/java/dev/java10x/cadastroNinjas/Ninjas/NinjaController.java
+++ cadastroNinjas/src/main/java/dev/java10x/cadastroNinjas/Ninjas/NinjaController.java
@@ -10,4 +10,41 @@ public class NinjaController {
     public String boasVindas(){
         return "Essa é minha primeria mensagem nessa rota";
     }
+
+
+    // CRUDE
+    // Criar novo ninja (CREATE)
+
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja criado";
+    }
+
+    // Mostrar todos os ninjas (READ)
+
+    @GetMapping("/mostrar")
+    public String MostrarTodosNinjas(){
+        return "Ninja exibido";
+    }
+    // Mostrar Ninja por ID (READ)
+
+    @GetMapping("/mostrarID")
+    public String MostrarTodosNinjasId(){
+        return "Mostrar Ninja por ID";
+    }
+
+
+    // Alterar dados do Ninjas (UPDATE)
+    @PutMapping("/alterar")
+    public String AlterarNinja(){
+        return "Ninja alterado";
+    }
+
+    // Deletar Ninjas (DELETE)
+
+    @DeleteMapping("/deletar")
+    public String DeletarNinja(){
+        return "Ninja Deletado";
+    }
+
 }
