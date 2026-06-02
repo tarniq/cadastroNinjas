6193a0a7ba8702fcafd0cdfc84072a975bc771ae
tarniq <thales.cuellas@gmail.com>
2026-06-01 21:31:34 -0300

    Chore: inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x dto:

--- cadastroNinjas/src/main/java/dev/java10x/cadastroNinjas/Ninjas/NinjaDTO.java
+++ cadastroNinjas/src/main/java/dev/java10x/cadastroNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,20 @@
 package dev.java10x.cadastroNinjas.Ninjas;
 
+import dev.java10x.cadastroNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private int idade;
+    private String email;
+    private String vila;
+    private MissoesModel missoes;
+
 }
--- cadastroNinjas/src/main/java/dev/java10x/cadastroNinjas/Ninjas/NinjaMapper.java
+++ cadastroNinjas/src/main/java/dev/java10x/cadastroNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,33 @@
 package dev.java10x.cadastroNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO){
+        NinjaModel ninjaModel = new NinjaModel();
+
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setVila(ninjaDTO.getVila());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel){
+        NinjaDTO ninjaDTO = new NinjaDTO();
+
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setVila(ninjaModel.getVila());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+
+        return ninjaDTO;
+    }
 }
